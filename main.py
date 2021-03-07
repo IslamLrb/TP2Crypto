e4a977572a1f41fb6f649e1ca2bb24f30fc12659
Larbi Abdelillah Islam <larbi.abdel27@gmail.com>
2021-03-07 12:51:11 +0100

    final

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 import hashlib
 
 def hacher(text):
-    #encoder la chaine de caractères
-    encoded_str = text.encode()
+     #encoder la chaine de caractères
+    texte_coder = text.encode()
 
-    #creation de l'objet sha1
-    hash_obj = hashlib.sha1(encoded_str)
+    #creation de l'objet sha256
+    objet_hacher = hashlib.sha1(texte_coder)
 
     #conversion de l'objet hacher en hexadécimal
-    hexV = hash_obj.hexdigest()
+    hexV = objet_hacher.hexdigest()
 
     return hexV
 
